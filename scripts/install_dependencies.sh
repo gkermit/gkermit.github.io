323a5ed1e605f0cf9ba0cef8478768b9ed5f453b
Greg Tracy <gtracy92@icloud.com>
2023-05-02 23:18:56 -0700

    Remove unused html, js, bash scripts

--- scripts/install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yum groupinstall -y "Web Server" "MySQL Database" "PHP Support"
-yum install -y php-mysql
--- scripts/stop_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-isExistApp=`pgrep httpd`
-if [[ -n  $isExistApp ]]; then
-   service httpd stop
-fi
-isExistApp=`pgrep mysqld`
-if [[ -n  $isExistApp ]]; then
-    service mysqld stop
-fi
